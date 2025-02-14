c150c8f94fff7ec0707866fff7eb01a5fee5ba46
Gabriel González Rojas <gabrielg13rojas@gmail.com>
2025-02-14 07:54:20 -0300

    DOCKERIZANDO

--- MabitAgro/src/main/java/py/com/mabit/repositorios/PrivilegiosRepositorio.java
+++ MabitAgro/src/main/java/py/com/mabit/repositorios/PrivilegiosRepositorio.java
@@ -1,7 +1,6 @@
 package py.com.mabit.repositorios;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,5 @@ import py.com.mabit.entidades.Privilegios;
 
 @Repository
 public interface PrivilegiosRepositorio extends JpaRepository<Privilegios, Long> {
-	
 	List<Privilegios> findByDescripcionIgnoreCaseContaining(String descripcion);
-
-	
 }
